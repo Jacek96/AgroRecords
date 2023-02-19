51df4584e6ab668d86b7bfbc627964260d934ce5
Jacek Ferdynus <jacekferdynus96@gmail.com>
2023-02-19 17:53:56 +0100

    Add plan of login page

--- lib/App/login/login_page.dart
+++ lib/App/login/login_page.dart
@@ -7,9 +7,54 @@ class LoginPage extends StatelessWidget {
 
   @override
   build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Jesteś niezalogowany'),
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: const [
+              SizedBox(height: 30),
+              // logo
+              Icon(
+                Icons.lock,
+                size: 100,
+              ),
+
+              SizedBox(height: 30),
+
+              // Welcome back, you've been missed
+              Text(
+                'Welcome back, you\'ve been missed',
+                style: TextStyle(fontSize: 16),
+              ),
+
+              SizedBox(height: 30),
+
+              //email textfield
+              TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                ),
+              ),
+
+              //password textfiled
+
+              // forgot password
+
+              // sign in button
+
+              // or continue with
+
+              // google + apple sign in buttons
+
+              // not a member? register now
+            ],
+          ),
+        ),
       ),
     );
   }
